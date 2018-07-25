9b0444fd475337c946bb4355f849bf4a389b7501
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-25 13:59:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,31 @@ var fs = require('fs');
 // -->COMMANDS<--
 
 bot.on('guildMemberAdd',member =>{
-    member.send('**İçerde Güzel Bir Ortam Var Ve Her Hafta 1 Çekiliş Oluyor Kaçırmayın Derim.** https://discord.gg/Ee7hBtV');
+    member.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "Sunucumuzda Sana İhtiyacımız Var",
+    url: "https://discord.gg/Ee7hBtV",
+    description: "Sunucumuza Gelip Bizi Desteklermisin",
+    fields: [{
+        name: "Sunucumuzda Her Hafta Çekiliş Oluyor Sende Katılmalısın!!",
+        value: "Örnek Olarak Steam Key,Steam Premium Key vs."
+      },
+      {
+        
+        name: "Eğer Sunucumuza Gelirsen @Recep | Fides'i Etiketle Sana Bir Kıyağı Olacaktır"
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Recep | Fides"
+    }
+  }
+})
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
 
