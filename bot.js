a0613ffc7f3a115336a51d2a413e0f6428f2c491
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-27 17:32:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 // -->COMMANDS<--
 
 bot.on('guildMemberAdd',member =>{
-    member.send('```Bu Sunucuyu Sevdiysen Bizim Sunucuyuda Gelirmisin İçerde Güzel Bir Ortam Var Ve Her Hafta 1 Çekiliş Oluyor Kaçırmayın Derim.``` \n```Kayıt Olmayı Sakın Unutmayın.```  \nhttps://discord.gg/Ee7hBtV \nhttps://media.giphy.com/media/oWjyixDbWuAk8/giphy.gif');
+    member.send('```Bu Sunucuyu Sevdiysen Bizim Sunucuyuda Gelirmisin İçerde Güzel Bir Ortam Var Ve Her Hafta 1 Çekiliş Oluyor Kaçırmayın Derim.``` https://discord.gg/Ee7hBtV ```Kayıt Olmayı Sakın Unutmayın.``` https://media.giphy.com/media/oWjyixDbWuAk8/giphy.gif');
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
    
@@ -15,7 +15,7 @@ bot.on('guildMemberAdd',member =>{
 
 
 bot.on('guildMemberRemove', member=>{
-    member.send('```Sunucudan Gidiyorsun Bari Bizim Sunucuyuda Gelirmisin İçerde Güzel Bir Ortam Var Ve Her Hafta 1 Çekiliş Oluyor Kaçırmayın Derim.``` \n```Kayıt Olmayı Sakın Unutmayın.```  \nhttps://discord.gg/Ee7hBtV \nhttps://media.giphy.com/media/oWjyixDbWuAk8/giphy.gif');
+    member.send('```Sunucudan Gidiyorsun Madem Bizim Sunucuyuda Gelirmisin İçerde Güzel Bir Ortam Var Ve Her Hafta 1 Çekiliş Oluyor Kaçırmayın Derim.``` https://discord.gg/Ee7hBtV ```Kayıt Olmayı Sakın Unutmayın.``` https://media.giphy.com/media/oWjyixDbWuAk8/giphy.gif');
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
    
