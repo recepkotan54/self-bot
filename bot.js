6d07c645cf6cdd5ed4151fed53114f881fcaba54
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-08-08 12:53:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,144 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const RichEmbed = new Discord.RichEmbed();
-var fs = require('fs');
+const fs = require('fs');
+const client = new Discord.Client();
 
-// -->COMMANDS<--
 
-bot.on('guildMemberAdd',member =>{
-    member.send('```Bu Sunucuyu Sevdiysen Bizim Sunucuyuda Gelirmisin İçerde Güzel Bir Ortam Var Ve Her Hafta 1 Çekiliş Oluyor Kaçırmayın Derim.``` https://discord.gg/p5PSTYC ```Kayıt Olmayı Sakın Unutmayın.``` https://media.giphy.com/media/oWjyixDbWuAk8/giphy.gif');
+client.on('ready', () => { // client (botumuz) hazır olduğunda
+  client.user.setActivity('Counter-Strike Global Offensive', { type: "PLAYING"}); // oynuyor kısmını "Seninle" yapsın
+  console.log(`Hazırım!`); // konsola "Hazırım" yazsın
+});
+client.on("guildMemberAdd", member => {
+    member.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC');
 
-    //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
-   
+    //member.guild.defaultChannel.sendMessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
+})
+client.on("guildMemberRemove", member => {
+    member.send(' Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC ');
+
+    //member.guild.defaultChannel.sendMessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
+})
+
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'sa') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'as') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'hb') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'hg') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'oç') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'partner') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'tmm') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'nasılsınız') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'hoşgeldin') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'hoşbuldum') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'hmm') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'tmm') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'partner olalımmı') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'by') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'ben gidiom') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'self bot') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'amk') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'aq') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+
+client.on("message", async message =>{
+  if (message.content.toLowerCase()=== 'hm') {
+message.author.send('Hey Sen! \nSeni ***__Simba__*** Krallığına Davet Ediyorum!:point_right:  https://discord.gg/p5PSTYC')
+  }
+})
+client.on("guildCreate", guild => {
+  console.log(`Yeni sunucuya katıldım: ${guild.name}\n. Bu sunucu ${guild.memberCount} üye!`);
+});
+client.on("guildDelete", guild => {
+  console.log(`Hey beni çıkardılar ;(: ${guild.name}`);
 });
 
 
 
-bot.on('guildMemberRemove', member=>{
-    member.send('```Sunucudan Gidiyorsun Madem Bizim Sunucuyuda Gelirmisin İçerde Güzel Bir Ortam Var Ve Her Hafta 1 Çekiliş Oluyor Kaçırmayın Derim.``` https://discord.gg/p5PSTYC ```Kayıt Olmayı Sakın Unutmayın.``` https://media.giphy.com/media/oWjyixDbWuAk8/giphy.gif');
-
-    //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
-   
-});
-
-
-
-bot.on('ready',()=> {
-console.log('Recep Fides Self Bot Bağlandı');
-
-});
-
-bot.login(process.env.BOT_TOKEN);
+client.login("process.env.BOT_TOKEN");
